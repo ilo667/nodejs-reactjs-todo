dc1d019f57db31f1de5f2127e302b73b2dc62274
ilo666777 <ilo666777@gmail.com>
2025-02-25 12:54:22 +0200

    connect to jawsdb

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,24 @@
 const { Sequelize } = require('sequelize');
 
+let sequelize;
+
 const DB_NAME = 'node-react-todo';
 const USER_NAME = 'root';
 const PASSWORD = 'Qwezxcasd1*';
 
-const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME || DB_NAME,
+        process.env.DB_USER || USER_NAME,
+        process.env.DB_PW || PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        },
+    );
+}
 
 module.exports = sequelize;
